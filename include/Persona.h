926bd4cad9e755c12938f9adb24fcb205789dfc6
AndreeCreatura <andreepoyo.creatura@gmail.com>
2026-09-16 18:34:14 -0300

    Added Header and CPP file for Persona

---
+++ include/Persona.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Persona {
+    private:
+    int ID;
+    string name;
+    int age;
+    string service;
+
+    public:
+
+    
+};
