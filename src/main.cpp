6b9aed8d3cfacb1f34e2da0fc004640f39e192bf
AndreeCreatura <andreepoyo.creatura@gmail.com>
2026-09-16 18:12:13 -0300

    Added correct file structure

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+
+int main() {
+
+    std::ifstream file("pacientes.txt");
+
+    if (!file.is_open()) {
+        std::cout << "File not found!" << std::endl;
+        return 1;
+    }
+    
+
+    std::string line;
+
+    while(std::getline(file, line)) { 
+        std::cout << line << std::endl;
+    }
+
+    file.close();
+
+
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
