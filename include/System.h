85c2acfecdd29019bd09b008f47d50fc678110df
AndreeCreatura <andreepoyo.creatura@gmail.com>
2026-09-19 13:36:32 -0300

    Fixed missing method in system header

--- include/System.h
+++ include/System.h
@@ -1,14 +1,15 @@
 #pragma once
 #include <string>
-using namespace std;
 
 
 class System {
     public:
-    virtual void loadPatients(string filePath) = 0;
+    virtual void loadPatients(std::string filePath) = 0;
     virtual void attendPatients(int amount) = 0;
     virtual void showServices() = 0;
     virtual void showHistory() = 0;
+
+    virtual std::string showWaiting() = 0;
 
     virtual ~System() = default;
 
